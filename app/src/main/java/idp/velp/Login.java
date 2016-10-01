f214eea24c6c4fbe0d3e197e45d1b91d3b6d8498
Jing Xiang <jxsim.2013@sis.smu.edu.sg>
2016-10-01 16:33:32 +0800

    commit branch

--- app/src/main/java/idp/velp/Login.java
+++ app/src/main/java/idp/velp/Login.java
@@ -2,6 +2,7 @@ package idp.velp;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class Login extends AppCompatActivity {
 
@@ -9,5 +10,6 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        Log.d("ddd","dddd");
     }
 }
